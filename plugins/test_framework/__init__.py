855705a1008a18224ef7524795750a62e80605b0
tech-explained <dayanandn0910@gmail.com>
2026-09-22 05:34:28 -0500

    add plugins/test_framework/__init__.py

---
+++ plugins/test_framework/__init__.py
@@ -0,0 +1,5 @@
+"""Metadata-driven ETL test framework: generic validators for GCS -> Dataflow -> Postgres pipelines.
+
+Importing this package has no Airflow / GCP dependencies so validators can be
+unit-tested anywhere.
+"""
